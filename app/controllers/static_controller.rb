5931be6762d994436e09e48893392cdede68db78
github username <github email address>
2020-04-27 23:49:05 -0700

    Done.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,2 +1,9 @@
 class StaticController < ApplicationController
+    # EXPLICIT RENDERING
+    # def
+    #     render "static/hello_world"
+    #     render "hello_world"
+    # end
+
+    # IMPLICIT RENDERING
 end
